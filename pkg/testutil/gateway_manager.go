594b0674d1aa269dcf1497839453f3745071a876
Edo Shor <edo@cloudinary.com>
2023-11-23 16:10:03 +0200

    Revert "try without websockets"
    This reverts commit e1a82e464dc92c5556c3557805517edc391c56dd.

--- pkg/testutil/gateway_manager.go
+++ pkg/testutil/gateway_manager.go
@@ -21,7 +21,7 @@ type GatewayManager struct {
 
 func (m *GatewayManager) Init() {
 	m.Config = &GatewayConfig{
-		GatewayURL:  "http://localhost:8088/",
+		GatewayURL:  "ws://localhost:8188/",
 		AdminURL:    "http://localhost:7088/admin",
 		AdminSecret: "janusoverlord",
 	}
