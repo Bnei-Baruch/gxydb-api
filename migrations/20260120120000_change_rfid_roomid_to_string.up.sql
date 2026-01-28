461cacefa5c44edfa8d13ce03b229164d37648cc
amnonbb <amnonbb@gmail.com>
2026-01-28 11:58:00 +0200

    Fix migration: drop FK constraint before changing room_id type, convert internal ID to gateway_uid

--- migrations/20260120120000_change_rfid_roomid_to_string.up.sql
+++ migrations/20260120120000_change_rfid_roomid_to_string.up.sql
@@ -1,3 +1,10 @@
+-- IMPORTANT: Drop FOREIGN KEY constraints first before changing types
+-- sessions.room_id will now store Janus room ID (gateway_uid), not internal rooms.id
+ALTER TABLE sessions DROP CONSTRAINT IF EXISTS sessions_room_id_fkey;
+
+-- Drop index on room_id (will recreate after type change)
+DROP INDEX IF EXISTS sessions_room_id_idx;
+
 -- Изменяем тип room_id в таблице sessions
 ALTER TABLE sessions ALTER COLUMN room_id TYPE varchar(255);
 
@@ -10,8 +17,22 @@ ALTER TABLE room_statistics ALTER COLUMN room_id TYPE varchar(255);
 -- Изменяем тип room_id в таблице composites_rooms
 ALTER TABLE composites_rooms ALTER COLUMN room_id TYPE varchar(255);
 
--- Обновляем существующие данные
-UPDATE sessions SET room_id = room_id::varchar(255);
-UPDATE sessions SET gateway_feed = gateway_feed::varchar(255);
-UPDATE room_statistics SET room_id = room_id::varchar(255);
-UPDATE composites_rooms SET room_id = room_id::varchar(255); 
+-- Обновляем существующие данные (конвертируем внутренний rooms.id в Janus gateway_uid)
+-- NOTE: This conversion assumes room_id currently contains rooms.id (internal ID)
+-- We need to convert it to gateway_uid (Janus room ID)
+UPDATE sessions s
+SET room_id = (SELECT r.gateway_uid::varchar FROM rooms r WHERE r.id = s.room_id::bigint)
+WHERE room_id IS NOT NULL AND room_id ~ '^\d+$';
+
+UPDATE sessions SET gateway_feed = gateway_feed::varchar(255) WHERE gateway_feed IS NOT NULL;
+
+UPDATE room_statistics rs
+SET room_id = (SELECT r.gateway_uid::varchar FROM rooms r WHERE r.id = rs.room_id::bigint)
+WHERE room_id IS NOT NULL AND room_id ~ '^\d+$';
+
+UPDATE composites_rooms cr
+SET room_id = (SELECT r.gateway_uid::varchar FROM rooms r WHERE r.id = cr.room_id::bigint)
+WHERE room_id IS NOT NULL AND room_id ~ '^\d+$';
+
+-- Recreate index on room_id (without FK constraint)
+CREATE INDEX IF NOT EXISTS sessions_room_id_idx ON sessions USING btree (room_id, created_at); 
